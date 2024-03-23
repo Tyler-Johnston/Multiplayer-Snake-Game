0165f6eb1bff6d49cef2391d8873ca42dee75936
Tyler Johnston <johnstontyler@protonmail.com>
2024-03-23 13:30:06 -0600

    first commit

---
+++ FinalProject - MultiplayerSnake/GameStateDemo.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace CS5410
+{
+    public class GameStateDemo : Game
+    {
+        private GraphicsDeviceManager m_graphics;
+        private IGameState m_currentState;
+        private Dictionary<GameStateEnum, IGameState> m_states;
+
+        public GameStateDemo()
+        {
+            m_graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            m_graphics.PreferredBackBufferWidth = 1920;
+            m_graphics.PreferredBackBufferHeight = 1080;
+
+            m_graphics.ApplyChanges();
+
+            // Create all the game states here
+            m_states = new Dictionary<GameStateEnum, IGameState>
+            {
+                { GameStateEnum.MainMenu, new MainMenuView() },
+                { GameStateEnum.GamePlay, new GamePlayView() },
+                { GameStateEnum.HighScores, new HighScoresView() },
+                { GameStateEnum.Help, new HelpView() },
+                { GameStateEnum.About, new AboutView() }
+            };
+
+            // Give all game states a chance to initialize, other than constructor
+            foreach (var item in m_states)
+            {
+                item.Value.initialize(this.GraphicsDevice, m_graphics);
+            }
+
+            // We are starting with the main menu
+            m_currentState = m_states[GameStateEnum.MainMenu];
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Give all game states a chance to load their content
+            foreach (var item in m_states)
+            {
+                item.Value.loadContent(this.Content);
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            GameStateEnum nextStateEnum = m_currentState.processInput(gameTime);
+
+            // Special case for exiting the game
+            if (nextStateEnum == GameStateEnum.Exit)
+            {
+                Exit();
+            }
+            else
+            {
+                m_currentState.update(gameTime);
+                m_currentState = m_states[nextStateEnum];
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            m_currentState.render(gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}
